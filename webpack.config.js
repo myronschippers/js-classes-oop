8a5097b3120ebd3f3cec79d58ba83d4311cb4e19
Myron <dubtwee@gmail.com>
2019-08-20 10:35:20 -0500

    setting up the webpack bundler to live reload for development

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
-const path = require('path');
+const HtmlWebPackPlugin = require( 'html-webpack-plugin' );
+const path = require( 'path' );
 
 module.exports = {
     // webpack configuration settings
+    context: __dirname,
     entry: './src/index.js', // kickoff location for Client
     output: {
-        path: `${__dirname}/dist`,
+        path: path.resolve( __dirname, 'dist' ),
         filename: 'isurus-app.bundle.js',
+        publicPath: '/',
     },
     devtool: 'inline-source-map',
     mode: 'development',
-    watch: true,
+    devServer: {
+       historyApiFallback: true,
+       port: 3000,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+           template: path.resolve( __dirname, 'public/index.html' ),
+           filename: 'index.html'
+        })
+     ]
 };
