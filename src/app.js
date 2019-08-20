a09936399717c524c1e9353aaf7bbd8012f9e4f6
Myron <dubtwee@gmail.com>
2019-08-20 09:41:43 -0500

    reduce testing scripts

--- src/app.js
+++
@@ -1,5 +0,0 @@
-const app = {
-    name: 'My Application',
-};
-
-export default app;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import app from './app';
-
-console.log('I\'m a little index...');
 console.log('Hello Isurus!!!');
-
-console.log(app);
